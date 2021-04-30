4608b0765f35b8b1db3e7707486f45e2963d8d20
Dire <diredeaths@gmail.com>
2021-04-29 18:27:25 -0700

    fix token list for hardhat

--- src/state/lists/hooks.ts
+++ src/state/lists/hooks.ts
@@ -98,7 +98,7 @@ function combineMaps(map1: TokenAddressMap, map2: TokenAddressMap): TokenAddress
     4: { ...map1[4], ...map2[4] },
     5: { ...map1[5], ...map2[5] },
     42: { ...map1[42], ...map2[42] },
-    1337: { ...map1[1], ...map2[1] }
+    1337: { ...map1[1337], ...map2[1337] }
   }
 }
 
