1de41827c39be8c301daffc6cb5289abf6dc0457
Juice <ju1cefr@gmail.com>
2021-05-17 20:58:44 -0700

    add cancel button to pending component

--- src/components/TransactionDiagnosis/index.tsx
+++ src/components/TransactionDiagnosis/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { usePendingTransactions } from "state/transactions/hooks"
-import styled from "styled-components"
+import styled from 'styled-components'
+import { usePendingTransactions, useTransactionCanceller } from 'state/transactions/hooks'
+import { TransactionDetails } from 'state/transactions/reducer'
+import { ButtonOutlined } from 'components/Button'
+import { useActiveWeb3React } from 'hooks'
 
 const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.bg1}
@@ -9,13 +12,35 @@ const Wrapper = styled.div`
 
 const StyledDiagnosticWrapper = styled.div``
 
+const StyledCancelButton = styled(ButtonOutlined)`
+  margin-top: 1.75rem;
+`
+
 export default function TransactionDiagnosis() {
   const pendingTransactions = usePendingTransactions()
+  const { chainId } = useActiveWeb3React()
+  const cancelTransaction = useTransactionCanceller()
+
+  function handleCancelClick(hash: string, tx: TransactionDetails) {
+    if (!chainId) return
+    if (!tx?.processed) return
+    cancelTransaction(
+      {
+        chainId,
+        hash
+      },
+      {
+        transaction: tx.processed
+      }
+    )
+  }
 
   return (
     <Wrapper>
       {Object.keys(pendingTransactions).map((hash) => {
         const tx = pendingTransactions[hash]
+        const canCancel = typeof tx?.status !== 'undefined'
+
         return (
           <StyledDiagnosticWrapper key={hash}>
             <h6>
@@ -30,6 +55,11 @@ export default function TransactionDiagnosis() {
             <div>
               Flashbots says... {tx.flashbotsResolution}
             </div>
+            {canCancel && (
+              <StyledCancelButton onClick={() => handleCancelClick(hash, tx)}>
+                Cancel Transaction
+              </StyledCancelButton>
+            )}
           </StyledDiagnosticWrapper>
         )
       })}
