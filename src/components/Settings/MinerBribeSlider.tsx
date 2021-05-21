7b6b6102941e2a836feb8751ad928e11520ffdb6
ZiggStardust <ziggstardust@protonmail.com>
2021-05-21 12:42:12 +0100

    miner bribe slider improvments

--- src/components/Settings/MinerBribeSlider.tsx
+++ src/components/Settings/MinerBribeSlider.tsx
@@ -1,39 +1,50 @@
 import React from 'react'
 import styled from 'styled-components'
+import { BribeEstimate } from '@alchemistcoin/sdk'
 import { useUserBribeMargin } from '../../state/user/hooks'
-import { MINER_BRIBE_MIN, MINER_BRIBE_MAX } from '../../constants'
+// import { MINER_BRIBE_MIN, MINER_BRIBE_MAX } from '../../constants'
 import Slider from './Slider'
+import useMinerBribeEstimate from '../../hooks/useMinerBribeEstimate'
 
 const Wrapper = styled.div`
   width: 100%;
+  padding: 0 2rem 5rem;
 `
 
-const LabelWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 1rem 0 0;
+// const LabelWrapper = styled.div`
+//   width: 100%;
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: space-between;
+//   padding: 1rem 0 0;
 
-  > div {
-    display: flex;
-    color: ${({ theme }) => theme.text1};
-    font-size: 0.8rem;
-  }
-`
+//   > div {
+//     display: flex;
+//     color: ${({ theme }) => theme.text1};
+//     font-size: 0.8rem;
+//   }
+// `
+
+// const SLIDER_VALUE_TO_METHOD_MAP: any = {
+//   '1': 'minBribe',
+//   '2': 'meanBribe',
+//   '3': 'maxBribe',
+//   '4': 'ASAP' // TODO - ??
+// }
 
 const MinderBribeSlider = () => {
+  const bribeEstimate: BribeEstimate | null = useMinerBribeEstimate()
   const [userBribeMargin, setUserBribeMargin] = useUserBribeMargin()
 
-  const onChange = (value: number) => setUserBribeMargin(value)
+  const onChange = (value: number) => {
+    setUserBribeMargin(value)
+  }
+  console.log('bribe estimate', bribeEstimate)
+  console.log('bribe userBribeMargin', userBribeMargin)
 
   return (
     <Wrapper>
-      <Slider min={MINER_BRIBE_MIN} max={MINER_BRIBE_MAX} step={1} value={userBribeMargin} onChange={onChange} />
-      <LabelWrapper>
-        <div>low success rates (low fees)</div>
-        <div>high success rate ($$)</div>
-      </LabelWrapper>
+      <Slider min={1} max={4} step={1} value={userBribeMargin} onChange={onChange} />
     </Wrapper>
   )
 }
