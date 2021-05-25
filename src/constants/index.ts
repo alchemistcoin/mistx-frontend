921e9be022a2e4fcaed7d7defe614b0375acee15
Dire <diredeaths@gmail.com>
2021-05-25 09:18:33 -0700

    fix isEthTrade, was enforcing eth balance on all trades

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -250,7 +250,7 @@ export const BLOCKED_ADDRESSES: string[] = [
 // Miner Bribe Margin
 // default bribe margin, in bips
 export const INITIAL_BRIBE_MARGIN = 2
-export const MINER_BRIBE_MIN = 5
+export const MINER_BRIBE_MIN = 25
 export const MINER_BRIBE_MAX = 200
 
 // The interval for manual transaction status checks are emitted if no update has been received for x seconds
