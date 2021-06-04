744007089b0ade46dd529791dbc1a09a75bd3278
Lint Action <lint-action@samuelmeuli.com>
2021-06-04 17:47:34 +0000

    Fix code style issues with ESLint

--- src/hooks/useWrapCallback.ts
+++ src/hooks/useWrapCallback.ts
@@ -44,7 +44,7 @@ export default function useWrapCallback(
           sufficientBalance && inputAmount
             ? async () => {
                 try {
-                  console.log('input amount', inputAmount);
+                  console.log('input amount', inputAmount)
                   const txReceipt = await wethContract.deposit({ value: `0x${inputAmount.raw.toString(16)}` })
                   addTransaction(txReceipt, {
                     summary: `Wrap ${inputAmount.toSignificant(6)} ETH to WETH`,
--- src/utils/maxAmountSpend.ts
+++ src/utils/maxAmountSpend.ts
@@ -6,10 +6,7 @@ import { MIN_ETH } from '../constants'
  * Given some token amount, return the max that can be spent of it
  * @param currencyAmount to return max of
  */
-export function maxAmountSpend(
-  currencyAmount?: CurrencyAmount,
-  wrapType?: WrapType
-): CurrencyAmount | undefined {
+export function maxAmountSpend(currencyAmount?: CurrencyAmount, wrapType?: WrapType): CurrencyAmount | undefined {
   if (!currencyAmount) return undefined
   if (currencyAmount.currency === ETHER && wrapType !== WrapType.NOT_APPLICABLE) {
     if (JSBI.greaterThan(currencyAmount.raw, MIN_ETH)) {
