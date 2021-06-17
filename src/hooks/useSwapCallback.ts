1f8d3926e03641fef7dc43ac659985174fb1f282
Juice <ju1cefr@gmail.com>
2021-06-16 18:08:17 -0700

    use isMetaMask to determine whether to use eth_sign or eth_signTransaction

--- src/hooks/useSwapCallback.ts
+++ src/hooks/useSwapCallback.ts
@@ -9,9 +9,9 @@ import isZero from '../utils/isZero'
 import { useActiveWeb3React } from './index'
 import useENS from './useENS'
 import { /*MISTX_RELAY_URI, */ INITIAL_ALLOWED_SLIPPAGE, ROUTER } from '../constants'
-// import { ethers } from 'ethers'
+import { ethers } from 'ethers'
 import { keccak256 } from 'ethers/lib/utils'
-// import { SignatureLike } from '@ethersproject/bytes'
+import { SignatureLike } from '@ethersproject/bytes'
 import { JsonRpcSigner, Web3Provider } from '@ethersproject/providers'
 import { useApproveCallbackFromTrade } from './useApproveCallback'
 // import { useEstimationCallback } from './useEstimationCallback'
@@ -108,9 +108,11 @@ export function useSwapCallback(
           web3Provider.provider.isMetaMask = false
         }
 
-        return approve()
-          .then(signedApproval => {
-            return contract.populateTransaction[methodName](...args, {
+        try {
+          const signedApproval = await approve()
+
+          try {
+            const populatedTx: PopulatedTransaction = await contract.populateTransaction[methodName](...args, {
               //modify nonce if we also have an approval
               nonce:
                 signedApproval === undefined
@@ -122,118 +124,119 @@ export function useSwapCallback(
               //gasLimit: calculateGasMargin(BigNumber.from(trade.estimatedGas)), //needed?
               ...(value && !isZero(value) ? { value } : {})
             })
-              .then(async populatedTx => {
-                //delete for serialize necessary
-                delete populatedTx.from
-                populatedTx.chainId = chainId
-                // const serialized = ethers.utils.serializeTransaction(populatedTx)
-                // const hash = keccak256(serialized)
 
-                // const encodedABI = (await contract.callStatic[methodName](...args)).encodeABI()
+            //delete for serialize necessary
+            delete populatedTx.from
+            populatedTx.chainId = chainId
 
-                return library
-                  .jsonRpcFetchFunc('eth_signTransaction', [
-                    {
-                      ...populatedTx,
-                      gasLimit: populatedTx.gasLimit?.toHexString(),
-                      gasPrice: '0x0',
-                      value: populatedTx.value?.toHexString()
-                    }
-                  ])
-                  .then((signedTx: SignedTransactionResponse) => {
-                    // console.log('signature', signature)
-                    //this returns the transaction & signature serialized and ready to broadcast
-                    //basically does everything that AD does with hexlify etc. - kek
-                    // const txWithSig = ethers.utils.serializeTransaction(populatedTx, signature)
-                    return { signedTx, populatedTx: populatedTx }
-                  })
-                  .finally(() => {
-                    if (web3Provider) {
-                      web3Provider.provider.isMetaMask = isMetamask
-                    }
-                  })
-                  .then(({ signedTx }: { signedTx: SignedTransactionResponse; populatedTx: PopulatedTransaction }) => {
-                    console.log('SIGNED TX', signedTx)
-                    const hash = keccak256(signedTx.raw)
-                    // const inputSymbol = trade.inputAmount.currency.symbol
-                    // const outputSymbol = trade.outputAmount.currency.symbol
-                    // const inputAmount = trade.inputAmount.toSignificant(3)
-                    // const outputAmount = trade.outputAmount.toSignificant(3)
-
-                    // const base = `Swap ${inputAmount} ${inputSymbol} for ${outputAmount} ${outputSymbol}`
-                    // const withRecipient =
-                    //   recipient === account
-                    //     ? base
-                    //     : `${base} to ${
-                    //         recipientAddressOrName && isAddress(recipientAddressOrName)
-                    //           ? shortenAddress(recipientAddressOrName)
-                    //           : recipientAddressOrName
-                    //       }`
-                    const swapReq: SwapReq = {
-                      amount0: args[0][0] as string,
-                      amount1: args[0][1] as string,
-                      path: args[0][2] as string[],
-                      to: args[0][3] as string,
-                      deadline: args[0][4]
-                    }
-
-                    const minerBribeBN = BigNumber.from(args[1])
-                    const estimatedEffectiveGasPriceBn = minerBribeBN.div(BigNumber.from(trade.estimatedGas))
-                    const estimatedEffectiveGasPrice = Number(formatUnits(estimatedEffectiveGasPriceBn, 'gwei'))
-
-                    const transactionReq: TransactionReq = {
-                      chainId,
-                      serializedApprove: signedApproval ? signedApproval : undefined,
-                      serializedSwap: signedTx.raw,
-                      swap: swapReq,
-                      bribe: args[1], // need to use calculated bribe
-                      routerAddress: ROUTER[trade.exchange],
-                      estimatedEffectiveGasPrice: estimatedEffectiveGasPrice,
-                      estimatedGas: Number(trade.estimatedGas),
-                      from: account
-                    }
-                    // console.log('trans req', transactionReq)
-                    // console.log('emit transaction', transactionReq)
-                    // send transaction via sockets here
-
-                    // we can't have TransactionResponse here
-                    // This can be handled by the socket method
-                    // addTransaction(
-                    //   { chainId, hash },
-                    //   {
-                    //     summary: withRecipient,
-                    //     trade
-                    //     //relay
-                    //   }
-                    // )
-
-                    //
-                    //
-                    //
-                    // if (relay) sendToRelay(relay.serializedApprove, relay.serializedSwap, relay.deadline)
-
-                    emitTransactionRequest(transactionReq)
-
-                    return hash
-                  })
-              })
-              .catch((error: any) => {
-                // if the user rejected the tx, pass this along
-                if (error?.code === 4001) {
-                  throw new Error('Transaction rejected.')
-                } else {
-                  // otherwise, the error was unexpected and we need to convey that
-                  console.error(`Swap failed`, error, methodName, args, value)
-                  throw new Error(`Swap failed: ${error.message}`)
+            // HANDLE METAMASK
+            // MetaMask does not support eth_signTransaction so we must use eth_sign as a workaround.
+            // For other wallets, use eth_signTransaction
+            let signedTx
+            if (isMetamask) {
+              const serialized = ethers.utils.serializeTransaction(populatedTx)
+              const hash = keccak256(serialized)
+              const signature: SignatureLike = await library.jsonRpcFetchFunc('eth_sign', [account, hash])
+              console.log('signature', signature)
+              // this returns the transaction & signature serialized and ready to broadcast
+              // basically does everything that AD does with hexlify etc. - kek
+              signedTx = ethers.utils.serializeTransaction(populatedTx, signature)
+            } else {
+              const signedTxRes: SignedTransactionResponse = await library.jsonRpcFetchFunc('eth_signTransaction', [
+                {
+                  ...populatedTx,
+                  gasLimit: populatedTx.gasLimit?.toHexString(),
+                  gasPrice: '0x0',
+                  value: populatedTx.value?.toHexString()
                 }
-              })
-          })
-          .catch((error: any) => {
-            console.error(`Approval failed`, error)
-            throw new Error(`Approval Failed: ${error.message}`)
-          })
+              ])
+
+              signedTx = signedTxRes.raw
+            }
+
+            // Set isMetaMask again after signing. (workaround for an issue with isMetaMask set on the provider during signing)
+            if (web3Provider) {
+              web3Provider.provider.isMetaMask = isMetamask
+            }
+
+            console.log('SIGNED TX', signedTx)
+            const hash = keccak256(signedTx)
+            // const inputSymbol = trade.inputAmount.currency.symbol
+            // const outputSymbol = trade.outputAmount.currency.symbol
+            // const inputAmount = trade.inputAmount.toSignificant(3)
+            // const outputAmount = trade.outputAmount.toSignificant(3)
+
+            // const base = `Swap ${inputAmount} ${inputSymbol} for ${outputAmount} ${outputSymbol}`
+            // const withRecipient =
+            //   recipient === account
+            //     ? base
+            //     : `${base} to ${
+            //         recipientAddressOrName && isAddress(recipientAddressOrName)
+            //           ? shortenAddress(recipientAddressOrName)
+            //           : recipientAddressOrName
+            //       }`
+            const swapReq: SwapReq = {
+              amount0: args[0][0] as string,
+              amount1: args[0][1] as string,
+              path: args[0][2] as string[],
+              to: args[0][3] as string,
+              deadline: args[0][4]
+            }
+
+            const minerBribeBN = BigNumber.from(args[1])
+            const estimatedEffectiveGasPriceBn = minerBribeBN.div(BigNumber.from(trade.estimatedGas))
+            const estimatedEffectiveGasPrice = Number(formatUnits(estimatedEffectiveGasPriceBn, 'gwei'))
+
+            const transactionReq: TransactionReq = {
+              chainId,
+              serializedApprove: signedApproval ? signedApproval : undefined,
+              serializedSwap: signedTx,
+              swap: swapReq,
+              bribe: args[1], // need to use calculated bribe
+              routerAddress: ROUTER[trade.exchange],
+              estimatedEffectiveGasPrice: estimatedEffectiveGasPrice,
+              estimatedGas: Number(trade.estimatedGas),
+              from: account
+            }
+            // console.log('trans req', transactionReq)
+            // console.log('emit transaction', transactionReq)
+            // send transaction via sockets here
+
+            // we can't have TransactionResponse here
+            // This can be handled by the socket method
+            // addTransaction(
+            //   { chainId, hash },
+            //   {
+            //     summary: withRecipient,
+            //     trade
+            //     //relay
+            //   }
+            // )
+
+            //
+            //
+            //
+            // if (relay) sendToRelay(relay.serializedApprove, relay.serializedSwap, relay.deadline)
+
+            emitTransactionRequest(transactionReq)
+
+            return hash
+          } catch (error) {
+            // if the user rejected the tx, pass this along
+            if (error?.code === 4001) {
+              throw new Error('Transaction rejected.')
+            } else {
+              // otherwise, the error was unexpected and we need to convey that
+              console.error(`Swap failed`, error, methodName, args, value)
+              throw new Error(`Swap failed: ${error.message}`)
+            }
+          }
+        } catch (error) {
+          console.error(`Approval failed`, error)
+          throw new Error(`Approval Failed: ${error.message}`)
+        }
       },
       error: null
     }
-  }, [trade, library, account, chainId, recipient, recipientAddressOrName, swapCall, approve, /*addTransaction*/])
+  }, [trade, library, account, chainId, recipient, recipientAddressOrName, swapCall, approve /*addTransaction*/])
 }
