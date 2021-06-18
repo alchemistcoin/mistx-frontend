d774c0f6655610e49e0b2bc2f05ba5aac8c082cb
Juice <ju1cefr@gmail.com>
2021-06-18 12:07:03 -0700

    remove console log

--- src/components/AccountDetails/index.tsx
+++ src/components/AccountDetails/index.tsx
@@ -325,7 +325,7 @@ export default function AccountDetails({
                     <WalletAction
                       style={{ fontSize: '.825rem', fontWeight: 600, marginRight: '8px' }}
                       onClick={() => {
-                        console.log('deactivate ledger', connector)
+                        // console.log('deactivate ledger', connector)
                         deactivate()
                       }}
                     >
