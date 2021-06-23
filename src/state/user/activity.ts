ea169f0baf785a9e01a941ea3b22e8b260e77897
ZiggStardust <ziggstardust@protonmail.com>
2021-06-23 22:54:59 +0100

    refresh page if idle for more than 24hrs

---
+++ src/state/user/activity.ts
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { AppState } from '../index'
+
+const Activity = () => {
+  const lastUpdateVersionTimestamp = useSelector<AppState, AppState['user']['lastUpdateVersionTimestamp']>(
+    state => state.user.lastUpdateVersionTimestamp
+  )
+
+  useEffect(() => {
+    document.addEventListener('visibilitychange', () => {
+      if (!document.hidden) {
+        const timeNow = new Date().getTime()
+        const timeDiffMins = Math.floor(Number(lastUpdateVersionTimestamp) - timeNow) / 1000 / 60
+        if (timeDiffMins > 1440) {
+          location.reload()
+        }
+      }
+    })
+  }, [])
+
+  return null
+}
+
+export default Activity
