996a9a518a43d8d26ad0ffaf74e85390be733f62
ju1cec <ju1cefr@gmail.com>
2021-07-24 15:47:01 -0700

    fix

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -350,6 +350,7 @@ export default function Sockets(): null {
     addPopup,
     dispatch,
     allTransactions,
+    getBundleByID,
     removeTransaction,
     updateTransaction,
     newAppVersionAvailable,
