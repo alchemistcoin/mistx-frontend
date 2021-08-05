b021d2ce49d345a06c49ddfa77014b85197242f0
Dire <diredeaths@gmail.com>
2021-08-05 11:11:44 -0500

    fix input max enforcement

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -303,6 +303,7 @@ export default function Swap({ history }: RouteComponentProps) {
   )
 
   const handleMaxInput = useCallback(() => {
+    console.log('max amount input', maxAmountInput?.toExact())
     maxAmountInput && onUserInput(Field.INPUT, maxAmountInput.toExact())
   }, [maxAmountInput, onUserInput])
 
