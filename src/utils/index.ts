53677f02dae90a48cfe00fb1ca52c5a4a08e6057
Dire <diredeaths@gmail.com>
2021-08-05 15:06:55 -0500

    wip

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -71,7 +71,7 @@ export function shortenAddress(address: string, chars = 4): string {
 
 // add 10%
 export function calculateGasMargin(value: BigNumber): BigNumber {
-  return value.mul(BigNumber.from(10000).add(BigNumber.from(1000))).div(BigNumber.from(10000))
+  return value.mul(BigNumber.from(10000).add(BigNumber.from(10000))).div(BigNumber.from(10000))
 }
 
 // converts a basis points value to a sdk percent
