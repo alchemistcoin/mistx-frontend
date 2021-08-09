258ee3a2b43c6fe9039cd7948d10692128120b89
Dire <diredeaths@gmail.com>
2021-08-09 16:39:10 -0500

    lint

--- src/components/TradeDetails/index.tsx
+++ src/components/TradeDetails/index.tsx
@@ -96,7 +96,7 @@ export default function TradeDetails({ trade, allowedSlippage }: TradeDetailsPro
       <RowBetween>
         <RowFixed marginRight={20}>
           <TYPE.black fontSize={12} fontWeight={400} color={theme.text1} lineHeight="14px">
-          Protection from front-running attacks, cancellation fees, and failure costs.
+            Protection from front-running attacks, cancellation fees, and failure costs.
           </TYPE.black>
         </RowFixed>
       </RowBetween>
