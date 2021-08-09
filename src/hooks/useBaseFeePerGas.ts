f6734ada3794adff1c1fecab85beb6ac8ccc0fb9
ZiggStardust <ziggstardust@protonmail.com>
2021-08-09 13:36:30 +0100

    state update fix

--- src/hooks/useBaseFeePerGas.ts
+++ src/hooks/useBaseFeePerGas.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { BigNumber } from '@ethersproject/bignumber'
 import useLatestBlockWithTransactions from './useLatestBlockWithTransactions'
 import { BASE_FEE_BLOCKS_IN_FUTURE } from '../constants'
@@ -17,11 +17,17 @@ export function getMaxBaseFeeInFutureBlock(baseFee: BigNumber, blocksInFuture: n
 export default function useBaseFeePerGas(): BigNumber | undefined {
   const [baseFee, setBaseFee] = useState<string | undefined>(undefined)
   const block = useLatestBlockWithTransactions()
+  const update = React.useRef<boolean>(true)
   useEffect(() => {
-    if (!block) {
-      setBaseFee(undefined)
-    } else {
-      setBaseFee(block.baseFeePerGas?.toString())
+    if (update) {
+      if (!block) {
+        setBaseFee(undefined)
+      } else {
+        setBaseFee(block.baseFeePerGas?.toString())
+      }
+    }
+    return () => {
+      update.current = false
     }
   }, [block])
   return useMemo(() => {
