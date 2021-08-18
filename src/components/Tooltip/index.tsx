8453be906f14a083979344ae44948a87b232f510
ZiggStardust <ziggstardust@protonmail.com>
2021-08-18 08:10:34 +0100

    revert test state back

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -43,7 +43,7 @@ function TooltipContent({ content, show, ...rest }: TooltipContentProps) {
 
 export function MouseoverTooltipContent({ content, children, ...rest }: Omit<TooltipContentProps, 'show'>) {
   const [show, setShow] = useState(true)
-  const open = useCallback(() => setShow(true), [setShow])
+  const open = useCallback(() => setShow(false), [setShow])
   const close = useCallback(() => setShow(false), [setShow])
   return (
     <TooltipContent {...rest} show={show} content={content}>
