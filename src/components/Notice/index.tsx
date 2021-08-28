7f807a42e6a58fc73b17f79ab8f8cf81a15fdc01
ZiggStardust <ziggstardust@protonmail.com>
2021-08-28 08:18:07 +0100

    notice adjustment

--- src/components/Notice/index.tsx
+++ src/components/Notice/index.tsx
@@ -6,20 +6,15 @@ export const Notice = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    const noticeShown = localStorage.getItem('rewardsNoticeV1')
-
-    if (!noticeShown) {
-      localStorage.setItem('rewardsNoticeV1', 'true')
-      dispatch(
-        addPopup({
-          content: {
-            message: 'You may receive some funds back in your wallet after a swap as we are beta testing a new feature.'
-          },
-          key: 'rewardsNoticeV1',
-          removeAfterMs: 20000
-        })
-      )
-    }
+    dispatch(
+      addPopup({
+        content: {
+          message: 'You may receive some funds back in your wallet after a swap as we are beta testing a new feature.'
+        },
+        key: 'rewardsNoticeV1',
+        removeAfterMs: 15000
+      })
+    )
   }, [dispatch])
 
   return null
