a4f423144dfd1a1e874a9012037a039f90b5537e
Dire <diredeaths@gmail.com>
2021-10-12 14:15:10 -0500

    remove gas amounts from tx payload

--- src/hooks/useSwapCallback.ts
+++ src/hooks/useSwapCallback.ts
@@ -2,7 +2,6 @@ import { PopulatedTransaction } from '@ethersproject/contracts'
 import { BigNumber } from '@ethersproject/bignumber'
 import { Trade, Currency, TradeType } from '@alchemist-coin/mistx-core'
 import { BundleReq, SwapReq, TransactionReq } from '@alchemist-coin/mistx-connect'
-import { formatUnits } from 'ethers/lib/utils'
 import { useMemo } from 'react'
 import { useTransactionAdder } from '../state/transactions/hooks'
 import { isAddress, shortenAddress } from '../utils'
@@ -179,15 +178,6 @@ export function useSwapCallback(
                       : recipientAddressOrName
                   }`
 
-            const minerBribeBN = BigNumber.from(args[1])
-            const totalFees = minerBribeBN
-            // if (baseFeePerGas && populatedTx.gasLimit) {
-            //   // totalFees = totalFees.add(baseFeePerGas.mul(trade.estimatedGas.toString()))
-            //   const requiredFunds = baseFeePerGas.mul(populatedTx.gasLimit).add(value)
-            //   console.log('requiredFunds', requiredFunds.toString())
-            // }
-            const estimatedEffectiveGasPriceBn = totalFees.div(BigNumber.from(trade.estimatedGas))
-            const estimatedEffectiveGasPrice = Number(formatUnits(estimatedEffectiveGasPriceBn, 'gwei'))
             const swapReq: SwapReq = {
               amount0: args[0][0] as string,
               amount1: args[0][1] as string,
@@ -197,8 +187,6 @@ export function useSwapCallback(
 
             // Create the transaction body with the serialized tx
             const transactionReq: TransactionReq = {
-              estimatedGas: Number(trade.estimatedGas),
-              estimatedEffectiveGasPrice,
               serialized: signedTx,
               raw: swapReq
             }
@@ -211,8 +199,6 @@ export function useSwapCallback(
             if (signedApproval) {
               // if there is an approval, create the Approval tx object
               const signedTransactionApproval: TransactionReq = {
-                estimatedGas: 25000,
-                estimatedEffectiveGasPrice: 0,
                 serialized: signedApproval,
                 raw: undefined
               }
