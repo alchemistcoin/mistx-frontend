9c59f857a15e3b7a1123398934d7d212394b96b3
ju1cec <ju1cecfr@gmail.com>
2021-11-19 00:32:21 -0800

    add additional error log

--- src/hooks/useSwapCallback.ts
+++ src/hooks/useSwapCallback.ts
@@ -248,7 +248,7 @@ export function useSwapCallback(
             } else {
               // otherwise, the error was unexpected and we need to convey that
               console.error(`Swap failed`, error, methodName, args, value)
-              throw new Error(`Swap failed: ${error.message}. ${error.stack}`)
+              throw new Error(`Swap failed: ${error.message}. ${error.stack}. ${error}`)
             }
           }
         } catch (error) {
